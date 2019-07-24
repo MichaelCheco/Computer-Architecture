687326594b63f7e01589da5c8ebae7d4ffb6d1c6
Michael <checomichael2@gmail.com>
2019-07-24 11:47:40 -0400

    work from yesterday

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,54 +8,62 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        self.pc = 0
         self.ram = [0] * 256
         self.reg = [0] * 8
-        self.pc = 0
+        self.reg[7] = 0xf4  # stack pointer
+
+    @property
+    def sp(self):
+        return self.reg[7]
+
+    @sp.setter
+    def sp(self, value):
+        self.reg[7] = value
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MDR, MAR):
+        self.ram[MAR] = MDR
 
     def load(self, path):
         """Load a program into memory."""
 
         address = 0
         program = []
-        # For now, we've just hardcoded a program:
 
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111,  # PRN R0
-        #     0b00000000,  # NOP - Do nothing
-        #     0b00000001,  # HLT
-        # ]
-        with open(path) as f:
-            for l in f:
-                instr = l.split("#", 1)[0].strip()
-                if len(instr):
-                    program.append(int(instr, 2))
+        try:
+            with open(path) as f:
+                for line in f:
+                    instr = line.split('#', 1)[0].strip()
+                    if len(instr):
+                        program.append(int(instr, 2))
+        except FileNotFoundError:
+            print(f"No file {path}, exiting")
+            sys.exit(2)
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
-
-    def ram_read(self, MAR):
-        MDR = self.ram[MAR]
-        return MDR
-
-    def ram_write(self, MAR, MDR):
-        self.ram[MAR] = MDR
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+
         elif op == "SUB":
-            pass
-        elif op == "LD":
-            pass
-        elif op == "ST":
-            pass
+            self.reg[reg_a] -= self.reg[reg_b]
+
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+
+        elif op == "AND":
+            self.reg[reg_a] &= self.reg[reg_b]
+
+        elif op == "OR":
+            self.reg[reg_a] |= self.reg[reg_b]
 
         else:
             raise Exception("Unsupported ALU operation")
@@ -82,18 +90,85 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        running = True
+        self.running = True
 
-        while running:
+        def halt():
+            self.running = False
+
+        def load_immediate():
+            self.reg[operand_a] = operand_b
+
+        def print_numeric():
+            print(self.reg[operand_a])
+
+        def store():
+            self.ram[operand_a] = self.reg[operand_b]
+
+        def add():
+            self.alu("ADD", operand_a, operand_b)
+
+        def sub():
+            self.alu("SUB", operand_a, operand_b)
+
+        def multiply():
+            self.alu("MUL", operand_a, operand_b)
+
+        def push_stack():
+            self.sp -= 1
+            self.ram[self.sp] = self.reg[operand_a]
+
+        def pop_stack():
+            self.reg[operand_a] = self.ram[self.sp]
+            self.sp += 1
+
+        def call():
+            self.sp -= 1
+            self.ram[self.sp] = self.ram[self.pc + 1]
+            self.pc = self.reg[operand_a]
+
+        def call_return():
+            self.pc = self.ram[self.sp]
+            self.sp += 1
+
+        def jump():
+            self.pc = self.reg[operand_a]
+
+        def bitwise_and():
+            self.alu("AND", operand_a, operand_b)
+
+        def bitwise_or():
+            self.alu("OR", operand_a, operand_b)
+
+        bt = {
+            0b00000001: halt,               # HTL
+            0b10000010: load_immediate,     # LDI
+            0b01000111: print_numeric,      # PRN
+            0b10000100: store,              # ST
+            0b10100000: add,                # ADD
+            0b10100001: sub,                # SUB
+            0b10100010: multiply,           # MUL
+            0b01000101: push_stack,         # PUSH
+            0b01000110: pop_stack,          # POP
+            0b01010000: call,               # CALL
+            0b00010001: call_return,        # RET
+            0b01010100: jump,               # JMP
+            0b10101000: bitwise_and,        # AND
+            0b10101010: bitwise_or,         # OR
+        }
+
+        while self.running:
             IR = self.ram[self.pc]
+            operand_count = (IR & 0b11000000) >> 6
+            sets_pc = (IR & 0b00010000) >> 4
             operand_a = self.ram[self.pc + 1]
             operand_b = self.ram[self.pc + 2]
-            if IR == 0b00000001:
-                running = False
-                self.pc += 1
-            elif IR == 0b10000010:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            elif IR == 0b01000111:
-                print(self.reg[operand_a])
-                self.pc += 2
+
+            command = bt.get(IR)
+
+            if not command:
+                print(f'Unknown instruction {IR:0b}')
+                sys.exit(1)
+
+            command()
+            if not sets_pc:
+                self.pc += (operand_count + 1)
